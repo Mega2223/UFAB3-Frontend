cad6efaf6a1bef05514f0c411b28ce2173866076
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2025-04-24 17:00:40 -0300

    Header Colors

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,41 @@ function App() {
     return (
         <>
             <div id = "header">
-                <img src={"./public/img.png"} alt={"Logo"}/>
-                <div id = "title-container">
-                    <p className="title">UFAB3</p>
+                <img src={"./UFAB3.png"} alt={"Logo"}/>
+                {/*<div id = "title-container">*/}
+                {/*    <p className="title">UFAB3</p>*/}
+                {/*</div>*/}
+            </div>
+            <div id = "center">
+                <div>
+                    <a href="https://vite.dev" target="_blank">
+                        <img src={viteLogo} className="logo" alt="Vite logo" />
+                    </a>
+                    <a href="https://react.dev" target="_blank">
+                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
                 </div>
-            </div>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                <h1>Vite + React</h1>
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to test HMR
+                    </p>
+                </div>
+                <p className="read-the-docs">
+                    E sorte é coisa além de cavalos e de sombras, coisas.
+                    Além de ficar ou de ir embora, a pele.
+                    Também vai embora quando morre o corpo.
+                    Morre a pele.
+                    Sobra vida.
+                    Somente pra quem curte o desvio.
                 </p>
             </div>
-            <p className="read-the-docs">
-                E sorte é coisa além de cavalos e de sombras, coisas.
-                Além de ficar ou de ir embora, a pele.
-                Também vai embora quando morre o corpo.
-                Morre a pele.
-                Sobra vida.
-                Somente pra quem curte o desvio.
-            </p>
+            <div id = "last">
+
+            </div>
         </>
     )
 }
