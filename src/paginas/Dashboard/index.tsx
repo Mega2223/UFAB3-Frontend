5aac94c17ee4905e77c0c0ebb3536ad9f4f1672f
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2025-04-27 21:45:27 -0300

    Tabela \o/

--- src/paginas/Dashboard/index.tsx
+++ src/paginas/Dashboard/index.tsx
@@ -1,10 +1,10 @@
 import {Header} from "../../App.tsx";
-import {Bottom} from "../../App.tsx";
 import "../../App.css"
 import {VictoryPie, VictoryTheme} from "victory";
 import React from "react";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {Paper} from "@mui/material";
+import { DataGrid , GridColDef} from '@mui/x-data-grid';
+
 
 import('./Dashboard.css');
 
@@ -24,6 +24,7 @@ function makeUpAssets(n: number){
     const ret: Asset[] = []
     for (let i = 0; i < n; i++) {
         const asset: Asset = {
+            id: i,
             ticker: 'petr4',
             name: 'Petrobras',
             pmed: '---',
@@ -41,79 +42,35 @@ function makeUpAssets(n: number){
 
 const stocks: Asset[] = makeUpAssets(5);
 
+const fields = [
+    'ticker','name','pmed','pat','diff','var_hj','var_tot','pct_ac','na_cart'
+]
+
 function genGridComponents(){
     const col: GridColDef[] = []
     for (let i = 0; i < fields.length; i++) {
-        col[i] = {field: fields[i], headername: fields[i], width: 70}
+        col[i] = {field: fields[i], headerName: fields[i], width: 70}
     }
     return col;
 }
 
-const columns: GridColDef[] = genGridComponents();
-
-const fields = [
-    'Ticker','Name','Pmed','Pat','Diff','var_hj','var_tot','pct_ac','na_cart'
-]
+const columns = genGridComponents();
+const rows = stocks;
+const paginationModel = { page: 0, pageSize: 5 };
 
 export function GenTable(){
     return (
-        <>
-        {/*<TableContainer>*/}
-        {/*    <Table sx={{  }} aria-label="simple table">*/}
-        {/*        <TableHead>*/}
-        {/*            <TableRow>*/}
-        {/*                <TableCell align="right">ticker</TableCell>*/}
-        {/*                <TableCell align="right">name</TableCell>*/}
-        {/*                <TableCell align="right">pmed</TableCell>*/}
-        {/*                <TableCell align="right">pat</TableCell>*/}
-        {/*                <TableCell align="right">diff</TableCell>*/}
-        {/*                <TableCell align="right">var_hj</TableCell>*/}
-        {/*                <TableCell align="right">var_tot</TableCell>*/}
-        {/*                <TableCell align="right">pct_ac</TableCell>*/}
-        {/*                <TableCell align="right">na_cart</TableCell>*/}
-        {/*            </TableRow>*/}
-        {/*        </TableHead>*/}
-        {/*        <TableBody>*/}
-        {/*            {stocks.map((row) => (*/}
-        {/*                <TableRow*/}
-        {/*                    key={row.name}*/}
-        {/*                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}*/}
-        {/*                >*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.ticker}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.name}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.pmed}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.pat}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.diff}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.var_hj}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.var_tot}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.pct_ac}*/}
-        {/*                    </TableCell>*/}
-        {/*                    <TableCell component="th" scope="row">*/}
-        {/*                        {row.na_cart}*/}
-        {/*                    </TableCell>*/}
-
-        {/*                </TableRow>*/}
-        {/*            ))}*/}
-        {/*        </TableBody>*/}
-        {/*    </Table>*/}
-        {/*</TableContainer>*/}
-        </>
-    )
+        <Paper sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                // checkboxSelection
+                sx={{ border: 0 }}
+            />
+        </Paper>
+    );
 }
 
 const Dashboard: React.FC = () => {
