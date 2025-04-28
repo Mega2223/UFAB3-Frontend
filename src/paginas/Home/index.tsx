b78019a39b41ae6d5261e06d29e24bd2157c7643
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2025-04-27 22:50:40 -0300

    Add buttons to navigate

--- src/paginas/Home/index.tsx
+++ src/paginas/Home/index.tsx
@@ -5,63 +5,66 @@ import GraphicPizzaExemple from './components/graphics/GraphicPizzaExemple';
 import PortfolioTable from './components/graphics/PortifolioTable';
 import { Bottom } from '../../App';
 
-
 const Home: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <>
-            <Header/>
-                <Toolbar/>
-                <Box
-                   display="flex"
-                   flexDirection="column"
-                   gap={10}
-                   alignItems="center"
-                   justifyContent="flex-start"
-                   marginTop="60px"
-                   minHeight="100vh"
-                   sx={{
-                      backgroundColor: 'white',
-                      paddingBottom: '20px',
-                      paddingTop: '100px', 
-                    }}
-                >
-                    <Box
-                        alignContent='center'
-                    >
-                        <Typography variant="h2" sx={{color: 'black'}}>
-                            Calcule seu imposto de renda
-                        </Typography>
-                        <Typography variant="h5" marginTop='10px' sx={{color: 'black'}}>
-                        Através de nosso sistema conseguimos fazer uma análise de seus ativos, comparando com meses anteriores e ajudando a fazer a declaração de seu imposto de renda.
-                        </Typography>
+      <Header />
+      <Toolbar />
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={10}
+        alignItems="center"
+        justifyContent="flex-start"
+        marginTop="60px"
+        minHeight="100vh"
+        sx={{
+          backgroundColor: 'white',
+          paddingBottom: '20px',
+          paddingTop: '100px',
+        }}
+      >
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/ir">Imposto de Renda</Link>
 
-                    </Box>
+        <Box alignContent="center">
+          <Typography variant="h2" sx={{ color: 'black' }}>
+            Calcule seu imposto de renda
+          </Typography>
+          <Typography variant="h5" marginTop="10px" sx={{ color: 'black' }}>
+            Através de nosso sistema conseguimos fazer uma análise de seus
+            ativos, comparando com meses anteriores e ajudando a fazer a
+            declaração de seu imposto de renda.
+          </Typography>
+        </Box>
 
-                    <Box
-                        alignContent='center'
-                    >
-                        <Typography variant="h3" sx={{color: 'black'}} alignContent='center' textAlign='center'>
-                            Visualize sua carteira
-                        </Typography>
-                        <GraphicPizzaExemple/>
+        <Box alignContent="center">
+          <Typography
+            variant="h3"
+            sx={{ color: 'black' }}
+            alignContent="center"
+            textAlign="center"
+          >
+            Visualize sua carteira
+          </Typography>
+          <GraphicPizzaExemple />
+        </Box>
 
-                    </Box>
-
-                    <Box
-                        alignContent='center'
-                    >
-                        <Typography variant="h3" sx={{color: 'black'}} alignContent='center' textAlign='center'>
-                            Analise seu portifólio 
-                        </Typography>
-                        <PortfolioTable/>
-
-                    </Box>
-            
-                </Box>
-        </>
-    
+        <Box alignContent="center">
+          <Typography
+            variant="h3"
+            sx={{ color: 'black' }}
+            alignContent="center"
+            textAlign="center"
+          >
+            Analise seu portifólio
+          </Typography>
+          <PortfolioTable />
+        </Box>
+      </Box>
+    </>
   );
 };
 
