de3aade2cd9476cea02f18357d4fa510f4d0f82e
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2025-04-29 08:10:11 -0300

    Small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,70 @@ import './App.css';
 import { useNavigate } from 'react-router-dom';
 
 const user = {
-    // TODO o user state foi migrado eu acredito
-    // name: "Júlio"
-    name:null
-}
+  // TODO o user state foi migrado eu acredito
+  // name: "Júlio"
+  name: null,
+};
 
 // let state = "main";
 
-export function LoginButton(){
-    const navigate = useNavigate();
-    const handleLoginClick = () => {
-        navigate('/login')
-    }
-    if(user.name == null){
-        return (<button id="login" onClick={handleLoginClick}>Login</button>)
-    } else {
-        return (<button id="user">{user.name}</button>)
-    }
+export function LoginButton() {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+  if (user.name == null) {
+    return (
+      <button id="login" onClick={handleLoginClick}>
+        Login
+      </button>
+    );
+  } else {
+    return <button id="user">{user.name}</button>;
+  }
 }
 
 export function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const login = LoginButton();
-    const handleHomeClick = () => {
-        navigate('/');
-      };
+  const login = LoginButton();
+  const handleHomeClick = () => {
+    navigate('/');
+  };
 
-
-    return (
-        <div id="header">
-            <img src={"/UFAB3.png"} alt={"Logo"}/>
-            <div id="buttons">
-                <button id="home" onClick={handleHomeClick}>Home</button>
-                {login}
-            </div>
-        </div>
-    )
+  return (
+    <div id="header">
+      <img src={'/UFAB3.png'} alt={'Logo'} />
+      <div id="buttons">
+        <button id="home" onClick={handleHomeClick}>
+          Home
+        </button>
+        {login}
+        {/* <button
+          id="dashboard"
+          onClick={() => {
+            navigate('/dashboard');
+          }}
+        >
+          Dashboard
+        </button> */}
+      </div>
+    </div>
+  );
 }
 
-export function Bottom(){
-    return (
-        <div id="bottom">
-            <img src={"/UFABC_LAZY.png"} id="socials" alt={"Logo UFAB3"}/>
-            <div id="buttons">
-                <button id="quem">Quem somos</button>
-                <button id="faq">FAQ</button>
-                <button id="email">Email</button>
-                <button id="fale">Fale Conosco</button>
-            </div>
-        </div>
-    )
+export function Bottom() {
+  return (
+    <div id="bottom">
+      <img src={'/UFABC_LAZY.png'} id="socials" alt={'Logo UFAB3'} />
+      <div id="buttons">
+        <button id="quem">Quem somos</button>
+        <button id="faq">FAQ</button>
+        <button id="email">Email</button>
+        <button id="fale">Fale Conosco</button>
+      </div>
+    </div>
+  );
 }
 
 // export function Body(){
--- src/paginas/uploadXlsx/index.tsx
+++ src/paginas/uploadXlsx/index.tsx
@@ -1,77 +1,86 @@
-import UploadButton from "./components/uploadButton"
-import {useNavigate} from "react-router";
-import {Box, Toolbar, Typography} from "@mui/material";
-import {Bottom, Header} from "../../App";
-import './Upload.css'
+import UploadButton from './components/uploadButton';
+import { useNavigate } from 'react-router';
+import { Box, Toolbar, Typography } from '@mui/material';
+import { Bottom, Header } from '../../App';
+import './Upload.css';
 
 const UploadXlsx: React.FC = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <Header/>
-            <div className="upload">
-            <Toolbar/>
-            <Box
-                display='flex'
-                flexDirection='column'
-                gap={10}
-                alignItems="center"
-                justifyContent="center"
-                marginTop='50px'
-                minHeight='100vh'
-                sx={{
-                    backgroundColor: 'white'
-                }}
-            >
-                <Box
-                    alignContent='center'
-                >
-                    <Typography variant="h2" sx={{color: 'black'}}>
-                        Realize o upload das transações
-                    </Typography>
-                    <Typography variant="h5" marginTop='10px' sx={{color: 'black'}}>
-                        Extraia as transações do portal de investidor da B3
-                    </Typography>
+  const navigate = useNavigate();
+  return (
+    <>
+      <Header />
+      <div className="upload">
+        <Toolbar />
+        <Box
+          display="flex"
+          flexDirection="column"
+          gap={10}
+          alignItems="center"
+          justifyContent="center"
+          marginTop="50px"
+          minHeight="100vh"
+          sx={{
+            backgroundColor: 'white',
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: '#dedede',
+              padding: '20px',
+              borderRadius: '8px',
+              ':hover': {
+                backgroundColor: '#c0c0c0',
+                cursor: 'pointer',
+              },
+            }}
+            onClick={() => {
+              navigate('/dashboard');
+            }}
+          >
+            <Typography variant="h4" sx={{ color: 'black' }}>
+              Acessar o Dashboard
+            </Typography>
+          </Box>
+          <Box alignContent="center">
+            <Typography variant="h2" sx={{ color: 'black' }}>
+              Realize o upload das transações
+            </Typography>
+            <Typography variant="h5" marginTop="10px" sx={{ color: 'black' }}>
+              Extraia as transações do portal de investidor da B3
+            </Typography>
 
-                    <UploadButton/>
+            <UploadButton />
+          </Box>
 
-                </Box>
+          <Box alignContent="center">
+            <Typography variant="h3" sx={{ color: 'black' }}>
+              Não sabe como extrair? Veja o Tutorial:
+            </Typography>
+          </Box>
 
-                <Box
-                    alignContent='center'
-                >
-                    <Typography variant="h3" sx={{color: 'black'}}>
-                        Não sabe como extrair? Veja o Tutorial:
-                    </Typography>
-
-                </Box>
-
-                <Box
-                    sx={{
-                        width: '50%',
-                        height: '300px',
-                        backgroundColor: '#f0f0f0',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        border: '2px dashed #888',
-                        borderRadius: '8px',
-                    }}
-                >
-                    <Typography variant="h6" color="text.secondary">
-                        Vídeo em breve
-                    </Typography>
-                </Box>
-
-            </Box>
-            </div>
-            <div className="upload-bottom">
-                <Bottom/>
-            </div>
-        </>
-    )
-
-
-}
+          <Box
+            sx={{
+              width: '50%',
+              height: '300px',
+              backgroundColor: '#f0f0f0',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              border: '2px dashed #888',
+              borderRadius: '8px',
+            }}
+          >
+            <Typography variant="h6" color="text.secondary">
+              Vídeo em breve
+            </Typography>
+          </Box>
+        </Box>
+      </div>
+      <div className="upload-bottom">
+        <Bottom />
+      </div>
+    </>
+  );
+};
 
 export default UploadXlsx;
